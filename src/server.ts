163c5eec964e320548230ca72809cd8c39bd8caa
victoralcan <victoralcan@gmail.com>
2021-09-15 17:16:35 -0300

    :wrench: Mudando porta do servidor

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log('Server started on port 3333!');
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server started on port 8080!');
 });
